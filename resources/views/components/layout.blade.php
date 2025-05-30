602496d779c7f08dc89d6d71b48cd6fd6c116023
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2025-05-30 18:08:47 +0100

    Tailwind configs

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
-    @vite(['resources/js.app.js'])
+    @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
 <body>
-    <div>
-        <nav>
+    <div class="px-10">
+        <nav class="flex justify-between items-center bg-red-500 py-4">
             <div>
                 <a href="">
                     <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
