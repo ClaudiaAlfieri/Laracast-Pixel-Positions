c9a8852aabf863980444492dea31b7e530d674c7
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2025-06-01 22:22:52 +0100

    Job-card & Section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,11 @@
 <x-layout>
     <section>
-        <h3 class="mb-6">Top Jobs</h3>
+        <x-section-heading>Feature Jobs</x-section-heading>
 
-        <div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
-            <div class="self-start text-sm">Laracasts</div>
-            <div class="py-8 font-bold">
-                <h3>Vídeo Producer</h3>
-                <p>Full Time - From $60,000</p>
-            </div>
-            <div class="flex justify-between items-center mt-auto">
-                <div>
-                    <a href="#" class="bg-whit/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
-                    <a href="#" class="bg-whit/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
-                    <a href="#" class="bg-whit/10 hover:bg-white/25 px-2 py-1 rounded-xl text-xs transition-colors duration-300">Tag</a>
-
-                </div>
-                <img src="http://placehold.it/42/42" alt="" class="rounded-xl">
-            </div>
+        <div class="grid lg:grid-cols-3 gap-8 mt-6">
+            <x-job-card></x-job-card>
+            <x-job-card></x-job-card>
+            <x-job-card></x-job-card>
         </div>
     </section>
 </x-layout>
