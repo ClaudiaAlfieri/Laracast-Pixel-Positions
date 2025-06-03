8ee945e5827c434c129728174e67f58b7b7d52db
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2025-06-03 11:07:39 +0100

    Create Routes

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,9 @@ use App\Http\Controllers\JobController;
 
 Route::get('/', [JobController::class, 'index'] );
 
+Route::get('/register', [RegisterUserController::class, 'create']);
+Route::post('/register', [RegisterUserController::class, 'store']);
 
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::delete('/logout', [SessionController::class, 'detroy']);
