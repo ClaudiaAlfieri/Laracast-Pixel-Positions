c1a7d0639d25aa52ad759532049a1afd653f7cd7
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2025-06-03 17:14:55 +0100

    Updates

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,2 +1,9 @@
-@props(['width'=>90])
-<img src="http://picsum.photos/seed/{{ rand(0,100000)}}/{{$width}}" alt="" class="rounded-xl">
+@props(['employer', 'width' => 90])
+
+<img src="{{ asset($employer->logo) }}" alt="" class="rounded-xl" width="{{ $width }}">
+
+
+
+{{-- Usar quando não tiver logos dos clientes: --}}
+{{-- @props(['width'=>90])
+<img src="http://picsum.photos/seed/{{ rand(0,100000)}}/{{$width}}" alt="" class="rounded-xl"> --}}
