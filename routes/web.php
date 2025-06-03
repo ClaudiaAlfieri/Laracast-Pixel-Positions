53b29606fa720747ff0e0581e5fc6e69f4680067
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2025-06-03 12:06:44 +0100

    Middleware

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,12 @@ use App\Http\Controllers\RegisteredUserController;
 
 Route::get('/', [JobController::class, 'index'] );
 
+Route::middleware('guest')->group(function(){
 Route::get('/register', [RegisteredUserController::class, 'create']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
 
 Route::get('/login', [SessionController::class, 'create']);
 Route::post('/login', [SessionController::class, 'store']);
-Route::delete('/logout', [SessionController::class, 'detroy']);
+});
+
+Route::delete('/logout', [SessionController::class, 'detroy'])->middleware('auth');
